bdff722355182b80e4d16268efaefe3f91f65c0e
sibgatullah-dev <sibgatullah881@gmail.com>
2025-12-10 17:46:31 +0600

    Django review

---
+++ Django the begining/mysite/members/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+# This is where we gather the information we need to send back a proper response. 
+
+def members(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('myfirst.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context,request))
