094b26266e7d7dec68686e89428c02bb2225844f
sibgatullah-dev <sibgatullah881@gmail.com>
2025-12-11 20:09:09 +0600

    set list display

--- Django the begining/mysite/members/admin.py
+++ Django the begining/mysite/members/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Member
 
 # Register your models here.
+'''We can control the fields to display by specifying them in a list_display property in the admin.py file.
+First create a MemberAdmin() class and specify the list_display tuple.
+'''
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ("firstname","lastname","joined_date")
+    
+admin.site.register(Member,MemberAdmin)
